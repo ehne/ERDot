6ee18364dce4ec521bfd1f27a63487c23a0514d6
Darcy LF <ehnevault@gmail.com>
2020-07-04 12:20:02 +1000

    remove bar

--- erdot/__main__.py
+++ erdot/__main__.py
@@ -6,18 +6,21 @@ import click
 import os
 import time
 
+
 def loadJson(_inputFile):
     with click.open_file(_inputFile) as f:
         jsonLoaded = json.load(f)
-    return(jsonLoaded)
+    return jsonLoaded
 
-def figureOutOutputFileName(_inputFile,_outputFile):
+
+def figureOutOutputFileName(_inputFile, _outputFile):
     outFileName = ""
     if _outputFile == "":
         outFileName = f"{_inputFile.replace('.erd', '').replace('.json', '')}.dot"
     else:
         outFileName = _outputFile
-    return(outFileName)
+    return outFileName
+
 
 def splitJSONIntoChuncks(_inJSON):
     try:
@@ -28,52 +31,49 @@ def splitJSONIntoChuncks(_inJSON):
     except:
         print("You are missing some information from your json file!")
         exit()
-    return([tables,relations,rankAdjustments,label])
+    return [tables, relations, rankAdjustments, label]
+
 
 def generateDotCode(_chunkedJSON):
     tpl = t(dTemp)
-    stringGen = tpl.render(tables=_chunkedJSON[0],
+    stringGen = tpl.render(
+        tables=_chunkedJSON[0],
         relations=_chunkedJSON[1],
         ra=_chunkedJSON[2],
         gs="",
-        lbl=_chunkedJSON[3])
-    return(stringGen)
+        lbl=_chunkedJSON[3],
+    )
+    return stringGen
+
 
 @click.command()
-@click.argument(
-    "inputfile", type=click.Path(exists=True)
-)
+@click.argument("inputfile", type=click.Path(exists=True))
 @click.option(
     "-o",
     "--outputFile",
     "o",
     help="The graphvis dot file to write to (.dot)",
-    default= ""
+    default="",
 )
 def main(inputfile, o):
     """ERDot generates graphvis .dot files from the .json file INPUTFILE."""
-    with click.progressbar(length=6, label='Geneating .dot file') as bar:
-        i = inputfile
-        # loads input json
-        jsonLoaded = loadJson(i)
-        bar.update(1)
-        # figure out the output file name
-        outFileName = figureOutOutputFileName(i,o)
-        bar.update(1)
-        # opens the file to write to
-        outputFile = click.open_file(outFileName, "w+")
-        bar.update(1)
-        # splits json into chunks
-        chunkedJSON = splitJSONIntoChuncks(jsonLoaded)
-        bar.update(1)
-        # generates the dot code
-        stringGen = generateDotCode(chunkedJSON)
-        bar.update(1)
-        # write dot code to output file
-        outputFile.write(stringGen)
-        bar.update(1)
-        #print(f"saved graphvis dot code to {outFileName}!")
+    i = inputfile
+    # loads input json
+    jsonLoaded = loadJson(i)
+    print(f"loaded {i}")
+    # figure out the output file name
+    outFileName = figureOutOutputFileName(i, o)
 
+    # opens the file to write to
+    outputFile = click.open_file(outFileName, "w+")
+    # splits json into chunks
+    chunkedJSON = splitJSONIntoChuncks(jsonLoaded)
+    # generates the dot code
+    stringGen = generateDotCode(chunkedJSON)
+    print(f"generated .dot code")
+    # write dot code to output file
+    outputFile.write(stringGen)
+    print(f"saved graphvis dot code to {outFileName}!")
 
 
 if __name__ == "__main__":
